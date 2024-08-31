4ca87a114ede099a0c949c0a508e08c6fc202f73
NivedhaRaghunath <manira1993@gmail.com>
2024-08-31 22:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from  streamlit_gsheets import GSheetsConnection
 
-# Google Sheets API setup
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/credentials.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open("Your Google Sheet Name").worksheet("expenses")
-
-# Fetch existing expense data
-existing_data = pd.DataFrame(sheet.get_all_records())
+# title and description
 
 st.title("Expenses Management Portal")
 st.markdown("Enter the details of Expenses")
 
+# Establish the gsheet connections
+conn = st.connection("gsheets",type = GSheetsConnection)
+
+# Fetch existing expense data
+existing_data = conn.read(worksheet="expenses",usecols=list(range(5)),ttl=5)
+existing_data = existing_data.dropna(how="all")
+
+#st.dataframe(existing_data)
+#List of expenses
+
 EXPENSES = ["Grocery","House_Hold","Fuel","Repair","Snacks","Pooja-Items"]
 MODE = ["Cash","Card","UPI"]
 
-with st.form(key="expenses_form"):
+with st.form(key = "expenses_form"):
     date = st.date_input(label="Select the date*")
-    expense_name = st.selectbox("Nature of Expense*", options=EXPENSES, index=None)
+    expense_name = st.selectbox("Nature of Expense*",options=EXPENSES,index=None)
     amount = st.number_input(label="Enter the amount*")
-    mode = st.selectbox("Select the mode of payment", options=MODE)
+    mode = st.selectbox("Select the mode of payment",options=MODE)
     remarks = st.text_area(label="Remarks")
 
     st.markdown("**required")
@@ -37,19 +38,27 @@ with st.form(key="expenses_form"):
             st.stop()
         else:
             expense_dict = [
-                {"DATE": date,
-                 "EXPENSE TYPE": expense_name,
-                 "AMOUNT": amount,
-                 "MODE OF PAYMENT": mode,
-                 "REMARKS": remarks
-                 }
-            ]
+                    {"DATE" : date,
+                     "EXPENSE TYPE" : expense_name,
+                     "AMOUNT" : amount,
+                     "MODE OF PAYMENT" : mode,
+                     "REMARKS" : remarks
+                     }
+                ]
             
             expense_df = pd.DataFrame(expense_dict)
+        
 
             # Add the new expense to the existing expense
-            updated_df = pd.concat([existing_data, expense_df], ignore_index=True)
 
-            # Update Google Sheets with the new expense data
-            sheet.append_rows(expense_df.values.tolist())
-            st.success("Expenses are updated successfully")
+            updated_df = pd.concat( [existing_data,expense_df],ignore_index=True)
+
+            # update google sheets with the new expense data
+
+            conn.update(worksheet="expenses",data=updated_df)
+            st.success("Expenses are updated succesfully")
+
+    
+
+
+
